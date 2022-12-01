02be09ff0c1caccb2919a7f3fd5cbb47e8a71375
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-01 00:48:58 -0500

    [Day 1] Sped up and simplified Rust

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,29 +1,30 @@
 fn main() {
-    let input = include_str!("../../../day01.txt");
+    let top_three_elves = include_str!("../../../day01.txt")
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|line| line.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .fold([0, 0, 0], |mut top_three_elves, new_elf| {
+            if new_elf > top_three_elves[1] {
+                top_three_elves[0] = top_three_elves[1];
+                if new_elf > top_three_elves[2] {
+                    top_three_elves[1] = top_three_elves[2];
+                    top_three_elves[2] = new_elf;
+                } else {
+                    top_three_elves[1] = new_elf;
+                }
+            } else if new_elf > top_three_elves[0] {
+                top_three_elves[0] = new_elf;
+            }
 
-    let part1 = input
-        .split("\n\n")
-        .map(|elf| {
-            elf
-                .lines()
-                .map(|line| line.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    let mut part2 = input
-        .split("\n\n")
-        .map(|elf| {
-            elf
-                .lines()
-                .map(|line| line.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
-    part2.sort();
-    part2.reverse();
-    let part2 = part2[0..3].iter().sum::<u32>();
-    
-    println!("Part 1: {part1}");
-    println!("Part 2: {part2}");
+            top_three_elves
+        });
+
+    let part_1 = top_three_elves[2];
+    let part_2 = part_1 + top_three_elves[1] + top_three_elves[0];
+
+    println!("Part 1: {part_1}");
+    println!("Part 2: {part_2}");
 }
