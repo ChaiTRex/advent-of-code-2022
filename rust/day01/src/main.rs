a49e6d03448deec24f9f4a25f6a5d04ca0f875c8
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-01 14:48:07 -0500

    [Day 1] Simplified Rust loop structure

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,26 +1,32 @@
 fn main() {
-    let top_three_elves = include_str!("../../../day01.txt")
-        .split("\n\n")
-        .map(|elf| {
-            elf.lines()
-                .map(|line| line.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .fold([0, 0, 0], |mut top_three_elves, new_elf| {
-            if new_elf > top_three_elves[1] {
-                top_three_elves[0] = top_three_elves[1];
-                if new_elf > top_three_elves[2] {
-                    top_three_elves[1] = top_three_elves[2];
-                    top_three_elves[2] = new_elf;
+    macro_rules! handle_new_elf {
+        ($top_three_elves: ident, $new_elf: ident) => {
+            if $new_elf > $top_three_elves[1] {
+                $top_three_elves[0] = $top_three_elves[1];
+                if $new_elf > $top_three_elves[2] {
+                    $top_three_elves[1] = $top_three_elves[2];
+                    $top_three_elves[2] = $new_elf;
                 } else {
-                    top_three_elves[1] = new_elf;
+                    $top_three_elves[1] = $new_elf;
                 }
-            } else if new_elf > top_three_elves[0] {
-                top_three_elves[0] = new_elf;
+            } else if $new_elf > $top_three_elves[0] {
+                $top_three_elves[0] = $new_elf;
             }
+        };
+    }
 
-            top_three_elves
-        });
+    let mut top_three_elves = [0, 0, 0];
+    let mut new_elf = 0;
+
+    for line in include_str!("../../../day01.txt").lines() {
+        if line.is_empty() {
+            handle_new_elf!(top_three_elves, new_elf);
+            new_elf = 0;
+        } else {
+            new_elf += line.parse::<u32>().unwrap();
+        }
+    }
+    handle_new_elf!(top_three_elves, new_elf);
 
     let part_1 = top_three_elves[2];
     let part_2 = part_1 + top_three_elves[1] + top_three_elves[0];
