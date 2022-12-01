4aa0e01330bf9033b70e0cfccd1810375c72ea3c
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-01 18:44:56 -0500

    Simplified loading of inputs in Haskell

--- haskell/AoC.hs
+++ haskell/AoC.hs
@@ -1,13 +1,40 @@
 module AoC where
 
+import Data.Array        (Array, (!), listArray)
 import Data.Word         (Word8)
 import System.IO         (readFile)
 import System.IO.Unsafe  (unsafePerformIO)
 
 {-# INLINE day #-}
 day :: Word8 -> String
-day d = let pathPrefix = if      d >=  1 && d <=  9 then "../day0"
-                         else if d >= 10 && d <= 25 then "../day"
-                         else                            error "`d` is not between 1 and 25"
-            path       = showString pathPrefix . shows d $ ".txt"
-        in  unsafePerformIO . readFile $ path
+day d = if d == 0 || d > 25 then error . shows d $ " is not between 1 and 25."
+        else                     unsafePerformIO . readFile $ filepaths ! d
+  where
+    filepaths :: Array Word8 String
+    filepaths = listArray (1, 25) [
+        "../day01.txt",
+        "../day02.txt",
+        "../day03.txt",
+        "../day04.txt",
+        "../day05.txt",
+        "../day06.txt",
+        "../day07.txt",
+        "../day08.txt",
+        "../day09.txt",
+        "../day10.txt",
+        "../day11.txt",
+        "../day12.txt",
+        "../day13.txt",
+        "../day14.txt",
+        "../day15.txt",
+        "../day16.txt",
+        "../day17.txt",
+        "../day18.txt",
+        "../day19.txt",
+        "../day20.txt",
+        "../day21.txt",
+        "../day22.txt",
+        "../day23.txt",
+        "../day24.txt",
+        "../day25.txt"
+      ]
