27ff1eca5615f95d217a980f2339cce5e55acb8e
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-01 00:11:03 -0500

    [Day 1] Added input, initial Rust solution

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,5 +1,29 @@
 fn main() {
     let input = include_str!("../../../day01.txt");
 
-    println!("{input}");
+    let part1 = input
+        .split("\n\n")
+        .map(|elf| {
+            elf
+                .lines()
+                .map(|line| line.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap();
+    let mut part2 = input
+        .split("\n\n")
+        .map(|elf| {
+            elf
+                .lines()
+                .map(|line| line.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<_>>();
+    part2.sort();
+    part2.reverse();
+    let part2 = part2[0..3].iter().sum::<u32>();
+    
+    println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
 }
