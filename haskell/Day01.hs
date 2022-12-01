7e76c4f1b73c2179ecd10a1d4a898a5b3b8534f5
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-01 17:58:03 -0500

    [Day 1] Added Haskell solution

--- haskell/Day01.hs
+++ haskell/Day01.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import AoC  (day)
+import AoC              (day)
+import Data.List        (foldl1', sortBy)
+import Data.List.Split  (splitWhen)
+import Data.Word        (Word32)
+
+{-# INLINE sum1 #-}
+sum1 :: [Word32] -> Word32
+sum1 = foldl1' (+)
 
 main :: IO ()
-main = print . day $ 1
+main = do
+    let xs = sortBy (flip compare) . map (sum1 . map read) . splitWhen null . lines . day $ 1
+    putStrLn . showString "Part 1: " . show . head $ xs
+    putStrLn . showString "Part 2: " . show . sum1 . take 3 $ xs
