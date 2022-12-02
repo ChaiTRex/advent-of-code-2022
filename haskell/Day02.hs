bd7e9736f2a365e022269dbfef7e7a2306273c6a
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-02 01:52:08 -0500

    [Day 2] Added input, Haskell solution, and Rust solution

--- haskell/Day02.hs
+++ haskell/Day02.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import AoC  (day)
+import AoC         (day)
+import Data.Array  (Array, (!), listArray)
+import Data.Char   (ord)
+import Data.List   (foldl1')
+import Data.Word   (Word16)
+
+{-# INLINE sum1 #-}
+sum1 :: [Word16] -> Word16
+sum1 = foldl1' (+)
+
+{-# NOINLINE partOneScores #-}
+partOneScores :: Array Int Word16
+partOneScores = listArray (0, 8) [4, 8, 3, 1, 5, 9, 7, 2, 6]
+
+{-# NOINLINE partTwoScores #-}
+partTwoScores :: Array Int Word16
+partTwoScores = listArray (0, 8) [3, 4, 8, 1, 5, 9, 2, 6, 7]
 
 main :: IO ()
-main = print . day $ 2
+main = do
+    let xs = map (\ [opponent, ' ', me] -> 3*ord opponent + ord me - 283) . lines . day $ 2
+    putStrLn . showString "Part 1: " . show . sum1 . map (partOneScores !) $ xs
+    putStrLn . showString "Part 2: " . show . sum1 . map (partTwoScores !) $ xs
--- rust/day02/src/main.rs
+++ rust/day02/src/main.rs
@@ -1,5 +1,26 @@
 fn main() {
-    let input = include_str!("../../../day02.txt");
+    static SCORES: [(u16, u16); 9] = [
+        (4, 3),
+        (8, 4),
+        (3, 8),
+        (1, 1),
+        (5, 5),
+        (9, 9),
+        (7, 2),
+        (2, 6),
+        (6, 7),
+    ];
 
-    println!("{input}");
+    let mut part_1 = 0;
+    let mut part_2 = 0;
+    for line in include_str!("../../../day02.txt").lines() {
+        let line = line.as_bytes();
+        let (part_1_score, part_2_score) = SCORES[3 * line[0] as usize + line[2] as usize - 283];
+
+        part_1 += part_1_score;
+        part_2 += part_2_score;
+    }
+
+    println!("Part 1: {part_1}");
+    println!("Part 2: {part_2}");
 }
