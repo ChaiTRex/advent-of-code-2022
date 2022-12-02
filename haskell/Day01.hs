04d916a1ad773bb530489f641d01b08b60894885
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-02 18:13:16 -0500

    [Day 1] Reduced Haskell sorting work

--- haskell/Day01.hs
+++ haskell/Day01.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
 import AoC              (day)
-import Data.List        (foldl1', sortBy)
+import Data.List        (foldl', foldl1', sort)
 import Data.List.Split  (splitWhen)
 import Data.Word        (Word32)
 
@@ -9,9 +11,21 @@ import Data.Word        (Word32)
 sum1 :: [Word32] -> Word32
 sum1 = foldl1' (+)
 
+{-# INLINE insertIntoTopList #-}
+insertIntoTopList :: [Word32] -> Word32 -> [Word32]
+insertIntoTopList tts@(!t : ts) !x = if t < x then go ts x else tts
+  where
+    go :: [Word32] -> Word32 -> [Word32]
+    go  tts@(!t : ts) !x = if t < x then t : go ts x else x : tts
+    go !tts           !x = x : tts
+
 main :: IO ()
 main = do
     input <- day 1
-    let xs = sortBy (flip compare) . map (sum1 . map read) . splitWhen null . lines $ input
-    putStrLn . showString "Part 1: " . show . head $ xs
-    putStrLn . showString "Part 2: " . show . sum1 . take 3 $ xs
+    let topThreeElves = foldl' insertIntoTopList [0, 0, 0]
+                      . map (sum1 . map read)
+                      . splitWhen null
+                      . lines
+                      $ input
+    putStrLn . showString "Part 1: " . show . last $ topThreeElves
+    putStrLn . showString "Part 2: " . show . sum1 $ topThreeElves
