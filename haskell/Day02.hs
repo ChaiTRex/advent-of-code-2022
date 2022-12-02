99f029ec2eb7b573476311b24b9f1014eb9e10e0
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-02 02:18:08 -0500

    [Day 2] Switched to unboxed arrays in Haskell

--- haskell/Day02.hs
+++ haskell/Day02.hs
@@ -1,21 +1,21 @@
 module Main where
 
-import AoC         (day)
-import Data.Array  (Array, (!), listArray)
-import Data.Char   (ord)
-import Data.List   (foldl1')
-import Data.Word   (Word16)
+import AoC                 (day)
+import Data.Array.Unboxed  (UArray, (!), listArray)
+import Data.Char           (ord)
+import Data.List           (foldl1')
+import Data.Word           (Word16)
 
 {-# INLINE sum1 #-}
 sum1 :: [Word16] -> Word16
 sum1 = foldl1' (+)
 
 {-# NOINLINE partOneScores #-}
-partOneScores :: Array Int Word16
+partOneScores :: UArray Int Word16
 partOneScores = listArray (0, 8) [4, 8, 3, 1, 5, 9, 7, 2, 6]
 
 {-# NOINLINE partTwoScores #-}
-partTwoScores :: Array Int Word16
+partTwoScores :: UArray Int Word16
 partTwoScores = listArray (0, 8) [3, 4, 8, 1, 5, 9, 2, 6, 7]
 
 main :: IO ()
