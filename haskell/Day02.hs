47ab6f77789858a5bffdc4e235f6d195f974b6d5
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-02 03:14:05 -0500

    [Day 2] Removed length is 3 and space character checks in Haskell

--- haskell/Day02.hs
+++ haskell/Day02.hs
@@ -20,6 +20,6 @@ partTwoScores = listArray (0, 8) [3, 4, 8, 1, 5, 9, 2, 6, 7]
 
 main :: IO ()
 main = do
-    let xs = map (\ [opponent, ' ', me] -> 3*ord opponent + ord me - 283) . lines . day $ 2
+    let xs = map (\ (opponent : _ : me : _) -> 3*ord opponent + ord me - 283) . lines . day $ 2
     putStrLn . showString "Part 1: " . show . sum1 . map (fromIntegral . (partOneScores !)) $ xs
     putStrLn . showString "Part 2: " . show . sum1 . map (fromIntegral . (partTwoScores !)) $ xs
