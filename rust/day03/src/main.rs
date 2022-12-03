71b5c3d95ba725aeab21b4da0983553c7ece1beb
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-03 03:51:20 -0500

    [Day 3] Reduced Rust LUT usage and size using idea from yescallop

--- rust/day03/src/main.rs
+++ rust/day03/src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
-    static PRIORITIES: [u8; 123] = {
-        let mut result = [0; 123];
+    static PRIORITIES: [u8; 58] = {
+        let mut result = [0; 58];
 
         let mut value = 1;
-        let mut ch = b'a';
-        while ch <= b'z' {
+        let mut ch = b'a' - b'A';
+        while ch <= b'z' - b'A' {
             result[ch as usize] = value;
 
             value += 1;
             ch += 1;
         }
 
-        ch = b'A';
-        while ch <= b'Z' {
+        ch = 0; // b'A' - b'A'
+        while ch <= b'Z' - b'A' {
             result[ch as usize] = value;
 
             value += 1;
@@ -40,18 +40,18 @@ fn main() {
             let (left_half, right_half) = line.split_at(line.len() >> 1);
             let mut left_set = 0_u64;
             for byte in left_half.iter().copied() {
-                left_set |= 1 << PRIORITIES[byte as usize];
+                left_set |= 1 << (byte - b'A');
             }
             let mut right_set = 0_u64;
             for byte in right_half.iter().copied() {
-                right_set |= 1 << PRIORITIES[byte as usize];
+                right_set |= 1 << (byte - b'A');
             }
 
-            part_1 += (left_set & right_set).trailing_zeros() as u16;
+            part_1 += PRIORITIES[(left_set & right_set).trailing_zeros() as usize] as u16;
             part_2_set &= left_set | right_set;
         }
 
-        part_2 += part_2_set.trailing_zeros() as u16;
+        part_2 += PRIORITIES[part_2_set.trailing_zeros() as usize] as u16;
     }
 
     println!("Part 1: {part_1}");
