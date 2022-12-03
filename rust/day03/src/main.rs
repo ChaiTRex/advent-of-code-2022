7816e75158a88a99cae8f00a74e680df43fb104f
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-03 02:29:52 -0500

    [Day 3] Simplified Rust loop

--- rust/day03/src/main.rs
+++ rust/day03/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    static VALUES: [u8; 123] = {
+    static PRIORITIES: [u8; 123] = {
         let mut result = [0; 123];
 
         let mut value = 1;
@@ -25,49 +25,41 @@ fn main() {
     let mut part_1 = 0;
     let mut part_2 = 0;
 
-    let mut part_2_set = 0_u64;
-    for (three_index, line) in (0..3).cycle().zip(
-        include_str!("../../../day03.txt")
-            .lines()
-            .map(str::as_bytes),
-    ) {
-        {
-            let (left_half, right_half) = line.split_at(line.len() >> 1);
-            let mut left_set = 0_u64;
-            for byte in left_half.iter().copied() {
-                left_set |= 1 << VALUES[byte as usize];
+    let mut lines = include_str!("../../../day03.txt")
+        .lines()
+        .map(str::as_bytes);
+    'outer: loop {
+        let mut part_2_set = !0_u64;
+
+        for _ in 0..3 {
+            let line = match lines.next() {
+                Some(line) => line,
+                None => break 'outer,
+            };
+
+            {
+                let (left_half, right_half) = line.split_at(line.len() >> 1);
+                let mut left_set = 0_u64;
+                for byte in left_half.iter().copied() {
+                    left_set |= 1 << PRIORITIES[byte as usize];
+                }
+                let mut right_set = 0_u64;
+                for byte in right_half.iter().copied() {
+                    right_set |= 1 << PRIORITIES[byte as usize];
+                }
+                part_1 += (left_set & right_set).trailing_zeros() as u16;
             }
-            let mut right_set = 0_u64;
-            for byte in right_half.iter().copied() {
-                right_set |= 1 << VALUES[byte as usize];
+
+            {
+                let mut part_2_line_set = 0_u64;
+                for byte in line.iter().copied() {
+                    part_2_line_set |= 1 << PRIORITIES[byte as usize];
+                }
+                part_2_set &= part_2_line_set;
             }
-            part_1 += (left_set & right_set).trailing_zeros() as u16;
         }
 
-        {
-            match three_index {
-                0 => {
-                    for byte in line.iter().copied() {
-                        part_2_set |= 1 << VALUES[byte as usize];
-                    }
-                }
-                1 => {
-                    let mut part_2_other_set = 0_u64;
-                    for byte in line.iter().copied() {
-                        part_2_other_set |= 1 << VALUES[byte as usize];
-                    }
-                    part_2_set &= part_2_other_set;
-                }
-                _ => {
-                    let mut part_2_other_set = 0_u64;
-                    for byte in line.iter().copied() {
-                        part_2_other_set |= 1 << VALUES[byte as usize];
-                    }
-                    part_2 += (part_2_set & part_2_other_set).trailing_zeros() as u16;
-                    part_2_set = 0;
-                }
-            }
-        }
+        part_2 += part_2_set.trailing_zeros() as u16;
     }
 
     println!("Part 1: {part_1}");
