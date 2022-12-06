3985440b45bc94d65a26c11c252f90bd8ac96b57
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-06 01:19:00 -0500

    [Day 6] Switched Rust character counts from `[u8; 26]` to `u128`

--- rust/day06/src/main.rs
+++ rust/day06/src/main.rs
@@ -1,27 +1,29 @@
 fn main() {
     let input = include_bytes!("../../../day06.txt");
-    let mut trailing_cursor = input.iter().copied().map(|ch| (ch - b'a') as usize);
+    let mut trailing_cursor = input.iter().copied().map(|ch| 1 << ((ch - b'a') << 2));
     let mut leading_cursor = (1..).zip(trailing_cursor.clone());
 
-    let mut seen = [0_u8; 26];
+    let mut char_counts = 0_u128;
 
     // The array constants are the increase in length minus one, so the length starts out
     // at 0, then increases by 4 (written as 3), and then increases by 10 (written as 9).
     let [part_1, part_2] = [3, 9].map(|prestretch| {
-        for (_, ch) in leading_cursor.by_ref().take(prestretch) {
-            seen[ch] += 1;
+        for (_, ch_bit) in leading_cursor.by_ref().take(prestretch) {
+            char_counts += ch_bit;
         }
 
         loop {
-            let (i, ch) = leading_cursor.next().unwrap();
-            seen[ch] += 1;
+            let (i, ch_bit) = leading_cursor.next().unwrap();
+            char_counts += ch_bit;
 
-            if seen.iter().copied().all(|count| count <= 1) {
+            // 0xe is 0b1110 and each character is given four bits, so this allows us to
+            // see when any of the characters has been seen more than once.
+            if char_counts & 0xee_eeee_eeee_eeee_eeee_eeee_eeee == 0 {
                 break i;
             }
 
-            let ch = trailing_cursor.next().unwrap();
-            seen[ch] -= 1;
+            let ch_bit = trailing_cursor.next().unwrap();
+            char_counts -= ch_bit;
         }
     });
 
