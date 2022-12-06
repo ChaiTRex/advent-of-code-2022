48a2dec864d921467d5fd4d9b793a5eb03b896d8
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-06 00:49:24 -0500

    [Day 6] Halved Rust code size

--- rust/day06/src/main.rs
+++ rust/day06/src/main.rs
@@ -5,37 +5,25 @@ fn main() {
 
     let mut seen = [0_u8; 26];
 
-    for (_, ch) in leading_cursor.by_ref().take(3) {
-        seen[ch] += 1;
-    }
+    // The array constants are the increase in length minus one, so the length starts out
+    // at 0, then increases by 4 (written as 3), and then increases by 10 (written as 9).
+    let [part_1, part_2] = [3, 9].map(|prestretch| {
+        for (_, ch) in leading_cursor.by_ref().take(prestretch) {
+            seen[ch] += 1;
+        }
 
-    let part_1 = loop {
-        let (i, ch) = leading_cursor.next().unwrap();
-        seen[ch] += 1;
+        loop {
+            let (i, ch) = leading_cursor.next().unwrap();
+            seen[ch] += 1;
 
-        if seen.iter().copied().all(|count| count <= 1) {
-            for (_, ch) in leading_cursor.by_ref().take(9) {
-                seen[ch] += 1;
+            if seen.iter().copied().all(|count| count <= 1) {
+                break i;
             }
 
-            break i;
+            let ch = trailing_cursor.next().unwrap();
+            seen[ch] -= 1;
         }
-
-        let ch = trailing_cursor.next().unwrap();
-        seen[ch] -= 1;
-    };
-
-    let part_2 = loop {
-        let (i, ch) = leading_cursor.next().unwrap();
-        seen[ch] += 1;
-
-        if seen.iter().copied().all(|count| count <= 1) {
-            break i;
-        }
-
-        let ch = trailing_cursor.next().unwrap();
-        seen[ch] -= 1;
-    };
+    });
 
     println!("Part 1: {part_1}");
     println!("Part 2: {part_2}");
