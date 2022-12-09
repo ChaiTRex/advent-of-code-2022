a8c57dba55595455e487ffad165061cf95b6bb74
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-09 01:09:59 -0500

    [Day 9] Rust is pretty simplified and efficient now

--- rust/day09/src/main.rs
+++ rust/day09/src/main.rs
@@ -3,72 +3,41 @@ use std::collections::HashSet;
 fn main() {
     let mut second_visited = HashSet::new();
     let mut tail_visited = HashSet::new();
-    
+
     let mut positions = [(0_isize, 0_isize); 10];
     for line in include_str!("../../../day09.txt").lines() {
         let mut iter = line.split(' ');
-        
-        let take_step = match iter.next().unwrap() {
-            "U" => {
-                fn up(head_position: &mut (isize, isize)) {
-                    head_position.1 += 1;
-                }
 
-                up
-            },
-            "D" => {
-                fn down(head_position: &mut (isize, isize)) {
-                    head_position.1 -= 1;
-                }
-
-                down
-            },
-            "L" => {
-                fn left(head_position: &mut (isize, isize)) {
-                    head_position.0 -= 1;
-                }
-
-                left
-            },
-            "R" => {
-                fn right(head_position: &mut (isize, isize)) {
-                    head_position.0 += 1;
-                }
-
-                right
-            },
+        let step_to_take = match iter.next().unwrap() {
+            "U" => (0, 1),
+            "D" => (0, -1),
+            "L" => (-1, 0),
+            "R" => (1, 0),
             _ => unreachable!(),
         };
-        
+
         for _ in 0..iter.next().unwrap().parse::<usize>().unwrap() {
-            take_step(&mut positions[0]);
-            
+            let head = &mut positions[0];
+            head.0 += step_to_take.0;
+            head.1 += step_to_take.1;
+
             for i in 0..9 {
-                match ((positions[i].0 - positions[i + 1].0).signum(), (positions[i].1 - positions[i + 1].1).signum()) {
-                    (0, 0) => (),
-                    (-1, 0) => if positions[i + 1].0 - positions[i].0 > 1 {
-                        positions[i + 1].0 -= 1;
-                    },
-                    (1, 0) => if positions[i].0 - positions[i + 1].0 > 1 {
-                        positions[i + 1].0 += 1;
-                    },
-                    (0, -1) => if positions[i + 1].1 - positions[i].1 > 1 {
-                        positions[i + 1].1 -= 1;
-                    },
-                    (0, 1) => if positions[i].1 - positions[i + 1].1 > 1 {
-                        positions[i + 1].1 += 1;
-                    },
-                    (a, b) => if positions[i].0.abs_diff(positions[i + 1].0) > 1 || positions[i].1.abs_diff(positions[i + 1].1) > 1 {
-                        positions[i + 1] = (positions[i + 1].0 + a, positions[i + 1].1 + b);
-                    }
+                let x = (positions[i].0 - positions[i + 1].0).signum();
+                let y = (positions[i].1 - positions[i + 1].1).signum();
+                if x * (positions[i].0 - positions[i + 1].0) > 1
+                    || y * (positions[i].1 - positions[i + 1].1) > 1
+                {
+                    let knot = &mut positions[i + 1];
+                    knot.0 += x;
+                    knot.1 += y;
                 }
             }
-            
+
             second_visited.insert(positions[1]);
             tail_visited.insert(positions[9]);
         }
     }
-    
+
     let part_1 = second_visited.len();
     let part_2 = tail_visited.len();
 
