f24744b3df00a33332c93b02e6d0368c7e22647d
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-12-09 00:58:23 -0500

    [Day 9] Handled both parts with the same code and stopped reparsing direction in Rust

--- rust/day09/src/main.rs
+++ rust/day09/src/main.rs
@@ -1,145 +1,75 @@
 use std::collections::HashSet;
 
 fn main() {
-    let input = include_str!("../../../day09.txt");
-
-    let mut head_position = (0_isize, 0_isize);
-    let mut tail_position = (0, 0);
-
+    let mut second_visited = HashSet::new();
     let mut tail_visited = HashSet::new();
-    for line in input.lines() {
-        let mut iter = line.split(' ');
-        let direction = iter.next().unwrap();
-        let distance = iter.next().unwrap().parse::<usize>().unwrap();
-
-        for _ in 0..distance {
-            match direction {
-                "U" => {
-                    let (x, y) = head_position;
-                    head_position = (x, y + 1);
-                }
-                "D" => {
-                    let (x, y) = head_position;
-                    head_position = (x, y - 1);
-                }
-                "L" => {
-                    let (x, y) = head_position;
-                    head_position = (x - 1, y);
-                }
-                "R" => {
-                    let (x, y) = head_position;
-                    head_position = (x + 1, y);
-                }
-                _ => unreachable!(),
-            }
-
-            match (
-                (head_position.0 - tail_position.0).signum(),
-                (head_position.1 - tail_position.1).signum(),
-            ) {
-                (0, 0) => (),
-                (-1, 0) => {
-                    if head_position.0.abs_diff(tail_position.0) > 1 {
-                        tail_position.0 -= 1;
-                    }
-                }
-                (1, 0) => {
-                    if head_position.0.abs_diff(tail_position.0) > 1 {
-                        tail_position.0 += 1;
-                    }
-                }
-                (0, -1) => {
-                    if head_position.1.abs_diff(tail_position.1) > 1 {
-                        tail_position.1 -= 1;
-                    }
-                }
-                (0, 1) => {
-                    if head_position.1.abs_diff(tail_position.1) > 1 {
-                        tail_position.1 += 1;
-                    }
-                }
-                (a, b) => {
-                    if head_position.0.abs_diff(tail_position.0) > 1
-                        || head_position.1.abs_diff(tail_position.1) > 1
-                    {
-                        tail_position = (tail_position.0 + a, tail_position.1 + b);
-                    }
-                }
-            }
-
-            tail_visited.insert(tail_position);
-        }
-    }
-
-    let part_1 = tail_visited.len();
-
+    
     let mut positions = [(0_isize, 0_isize); 10];
-
-    let mut tail_visited = HashSet::new();
-    for line in input.lines() {
+    for line in include_str!("../../../day09.txt").lines() {
         let mut iter = line.split(' ');
-        let direction = iter.next().unwrap();
-        let distance = iter.next().unwrap().parse::<usize>().unwrap();
+        
+        let take_step = match iter.next().unwrap() {
+            "U" => {
+                fn up(head_position: &mut (isize, isize)) {
+                    head_position.1 += 1;
+                }
 
-        for _ in 0..distance {
-            match direction {
-                "U" => {
-                    let (x, y) = positions[0];
-                    positions[0] = (x, y + 1);
+                up
+            },
+            "D" => {
+                fn down(head_position: &mut (isize, isize)) {
+                    head_position.1 -= 1;
                 }
-                "D" => {
-                    let (x, y) = positions[0];
-                    positions[0] = (x, y - 1);
-                }
-                "L" => {
-                    let (x, y) = positions[0];
-                    positions[0] = (x - 1, y);
-                }
-                "R" => {
-                    let (x, y) = positions[0];
-                    positions[0] = (x + 1, y);
-                }
-                _ => unreachable!(),
-            }
 
+                down
+            },
+            "L" => {
+                fn left(head_position: &mut (isize, isize)) {
+                    head_position.0 -= 1;
+                }
+
+                left
+            },
+            "R" => {
+                fn right(head_position: &mut (isize, isize)) {
+                    head_position.0 += 1;
+                }
+
+                right
+            },
+            _ => unreachable!(),
+        };
+        
+        for _ in 0..iter.next().unwrap().parse::<usize>().unwrap() {
+            take_step(&mut positions[0]);
+            
             for i in 0..9 {
-                match (
-                    (positions[i].0 - positions[i + 1].0).signum(),
-                    (positions[i].1 - positions[i + 1].1).signum(),
-                ) {
+                match ((positions[i].0 - positions[i + 1].0).signum(), (positions[i].1 - positions[i + 1].1).signum()) {
                     (0, 0) => (),
-                    (-1, 0) => {
-                        if positions[i].0.abs_diff(positions[i + 1].0) > 1 {
-                            positions[i + 1].0 -= 1;
-                        }
-                    }
-                    (1, 0) => {
-                        if positions[i].0.abs_diff(positions[i + 1].0) > 1 {
-                            positions[i + 1].0 += 1;
-                        }
-                    }
-                    (0, -1) => {
-                        if positions[i].1.abs_diff(positions[i + 1].1) > 1 {
-                            positions[i + 1].1 -= 1;
-                        }
-                    }
-                    (0, 1) => {
-                        if positions[i].1.abs_diff(positions[i + 1].1) > 1 {
-                            positions[i + 1].1 += 1;
-                        }
-                    }
-                    (a, b) => {
-                        if positions[i].0.abs_diff(positions[i + 1].0) > 1
-                            || positions[i].1.abs_diff(positions[i + 1].1) > 1
-                        {
-                            positions[i + 1] = (positions[i + 1].0 + a, positions[i + 1].1 + b);
-                        }
+                    (-1, 0) => if positions[i + 1].0 - positions[i].0 > 1 {
+                        positions[i + 1].0 -= 1;
+                    },
+                    (1, 0) => if positions[i].0 - positions[i + 1].0 > 1 {
+                        positions[i + 1].0 += 1;
+                    },
+                    (0, -1) => if positions[i + 1].1 - positions[i].1 > 1 {
+                        positions[i + 1].1 -= 1;
+                    },
+                    (0, 1) => if positions[i].1 - positions[i + 1].1 > 1 {
+                        positions[i + 1].1 += 1;
+                    },
+                    (a, b) => if positions[i].0.abs_diff(positions[i + 1].0) > 1 || positions[i].1.abs_diff(positions[i + 1].1) > 1 {
+                        positions[i + 1] = (positions[i + 1].0 + a, positions[i + 1].1 + b);
                     }
                 }
             }
+            
+            second_visited.insert(positions[1]);
             tail_visited.insert(positions[9]);
         }
     }
+    
+    let part_1 = second_visited.len();
     let part_2 = tail_visited.len();
 
     println!("Part 1: {part_1}");
